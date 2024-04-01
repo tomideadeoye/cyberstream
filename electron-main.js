94ccc64723844c092bac00afa95a5a11d62e10bd
Tomide Adeoye <55337742+tomideadeoye@users.noreply.github.com>
2024-04-01 20:39:58 +0100

    stream update

--- electron-main.js
+++ electron-main.js
@@ -1,10 +1,10 @@
-import path from "path";
-
 import { app, BrowserWindow } from "electron";
-import isDev from "electron-is-dev";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 function createWindow() {
-  // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -12,36 +12,20 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
-  mainWindow.webContents.openDevTools({ mode: "bottom" });
-  // and load the index.html of the app.
-  // win.loadFile("index.html");
-  win.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
-  // Open the DevTools.
-  if (isDev) {
-    win.webContents.openDevTools({ mode: "detach" });
-  }
+  win.loadURL(join(__dirname, "index.html"));
 }
+app.whenReady().then(() => {
+  createWindow();
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bars to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
-  }
-});
-
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
   }
 });
